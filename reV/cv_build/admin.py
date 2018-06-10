643784937801ddd5d1618344549049b2fd1a2715
Zachary Sizemore <zsizemore2320@gmail.com>
2018-06-10 18:55:26 -0400

    basic model setup
    added resume, work history, position details, skills classes

--- reV/cv_build/admin.py
+++ reV/cv_build/admin.py
@@ -1,3 +1,8 @@
+import inspect
+from . import models
 from django.contrib import admin
 
-# Register your models here.
+for name, obj in inspect.getmembers(models):
+    if inspect.isclass(obj):
+        admin.site.register(obj)
+
--- reV/reV/settings.py
+++ reV/reV/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'cv_build.apps.CvBuildConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
