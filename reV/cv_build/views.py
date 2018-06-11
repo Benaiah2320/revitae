62c020d0bfeabcb6a34232a02879ce7ec2d10dd8
Zachary Sizemore <zsizemore2320@gmail.com>
2018-06-10 21:14:24 -0400

    basic test

--- reV/cv_build/views.py
+++ reV/cv_build/views.py
@@ -1,9 +1,9 @@
 #cv_build/views
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Resume
 
-def resume(reuest, resume_id):
+def resume(request, resume_id):
 
     resume = get_object_or_404(Resume, pk=resume_id)
 
